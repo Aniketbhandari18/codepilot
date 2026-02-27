2fe06f6773a5ae10cdb12e4a88846fe32ae43bc4
Aniket Bhandari <aniketbhandari1807@gmail.com>
2026-02-27 18:31:58 +0530

    Fix: MainLayout pt-16 conditional class and correct Tailwind button colors

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -19,7 +19,9 @@ export default function MainLayout({
   return (
     <div className="min-h-screen flex flex-col">
       {!shouldHideNavbar && Navbar}
-      <main className={`${!shouldHideNavbar && "pt-16"} flex-1 flex flex-col`}>
+      <main
+        className={`${!shouldHideNavbar ? "pt-16" : ""} flex-1 flex flex-col`}
+      >
         {children}
       </main>
     </div>
